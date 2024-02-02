d4f634c58d65b0e82b139e6ba2f2646ffa62a2c8
hey365 <37896977+hey365@users.noreply.github.com>
2024-02-02 10:24:44 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,6 +10,7 @@ sed -i 's/192.168.1.1/192.168.3.5/g' openwrt/package/base-files/files/bin/config
 sed -i '175i\set network.$1.gateway='192.168.3.1'\
 set network.$1.dns='192.168.3.1'\
 ' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/option lang auto/option lang zh_cn/g' openwrt/feeds/luci/modules/luci-base/root/etc/config/luci
 
 #2. Clear the login password
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
