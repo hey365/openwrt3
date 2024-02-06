87fd5db0917056a96789c09e9c4704cededf2f6e
hey365 <37896977+hey365@users.noreply.github.com>
2024-02-06 16:42:46 +0800

    Update audo1.sh

--- audo1.sh
+++ audo1.sh
@@ -41,8 +41,8 @@ groupadd opwrt
 sudo useradd -g opwrt -d /home/opwrt -s /bin/bash -m opwrt && echo 'opwrt:Q%sdt854%' | sudo chpasswd
 
 #把文件移动到opwrt用户的主目录下
-mv audo2.sh ../
-mv !(audo1.sh) /home/opwrt/ && sudo chown -R opwrt /home/opwrt/
+mv audo1.sh ../ && mv audo2.sh ../
+mv ./* /home/opwrt/ && sudo chown -R opwrt /home/opwrt/
 
 #重启
 reboot
