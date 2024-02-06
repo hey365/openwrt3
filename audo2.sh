ef551230cc87d6919edf2f92da8b2719f28b5657
hey365 <37896977+hey365@users.noreply.github.com>
2024-02-06 21:13:46 +0800

    Update audo2.sh

--- audo2.sh
+++ audo2.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
-#安装screen并新建名为test的窗口
-sudo apt install screen
+手动执行下列命令
+sudo apt install screen && screen -R test
 
-#创建新窗口
-screen -S my_screen -r screen
+su -l opwrt && ./audo3.sh
 
-#切换用户
-
-screen -r my_screen -p 0 -X stuff "su opwrt$(printf \\r)"
-
-#运行audo3.sh
-
-screen -r my_screen -p 0 -X stuff "cd ~ && ./audo3.sh$(printf \\r)"
