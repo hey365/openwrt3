775665e0dc2441694b2d91968bcc5fdfeb58bdfe
hey365 <37896977+hey365@users.noreply.github.com>
2024-04-28 22:40:40 +0800

    Update add_luci.sh

--- add_luci.sh
+++ add_luci.sh
@@ -23,14 +23,13 @@ git pull --depth 1 origin master
 # 修改权限
 find ./applications/ -type f -exec chmod +x {} \;
 #修改kodexplorer的Makefile
-sed -i '/include \.\.\/\.\.\/luci\.mk/d' applications/luci-app-kodexplorer/Makefile
-sed -i '$i\include $(TOPDIR)/feeds/luci/luci.mk' applications/luci-app-kodexplorer/Makefile
+sed -i '/include \.\.\/\.\.\/luci\.mk/d' applications/luci-app-kodexplorer/Makefile && sed -i '$i\include $(TOPDIR)/feeds/luci/luci.mk' applications/luci-app-kodexplorer/Makefile
 
-sed -i '/include \.\.\/\.\.\/luci\.mk/d' applications/luci-app-diskman/Makefile
-sed -i '$i\include $(TOPDIR)/feeds/luci/luci.mk' applications/luci-app-diskman/Makefile
 
-sed -i '/include \.\.\/\.\.\/luci\.mk/d' applications/luci-app-vlmcsd/Makefile
-sed -i '$i\include $(TOPDIR)/feeds/luci/luci.mk' applications/luci-app-vlmcsd/Makefile
+sed -i '/include \.\.\/\.\.\/luci\.mk/d' applications/luci-app-diskman/Makefile && sed -i '$i\include $(TOPDIR)/feeds/luci/luci.mk' applications/luci-app-diskman/Makefile
+
+sed -i '/include \.\.\/\.\.\/luci\.mk/d' applications/luci-app-vlmcsd/Makefile && sed -i '$i\include $(TOPDIR)/feeds/luci/luci.mk' applications/luci-app-vlmcsd/Makefile
+
 # 现在把它移动到openwrt的pakcage文件夹下
 
  mv -f ./applications/* ../openwrt/package/
